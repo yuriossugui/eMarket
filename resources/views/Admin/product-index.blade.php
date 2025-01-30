672a7043abfc87f35be3932a1ec5b194168770e6
yuri ossugui <yuriossugui2@gmail.com>
2025-01-30 17:11:46 -0300

    style: product-index main components

--- resources/views/Admin/product-index.blade.php
+++ resources/views/Admin/product-index.blade.php
@@ -2,16 +2,57 @@
 
 @section('title', 'Product Index')
 
-@section('content_header')
-    <h1>PRODUCT INDEX</h1>
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 @endsection
 
-@section('content')
-    <a href="{{ route('logout') }}">Sair</a>
 
-    <!-- checks if are authenticated -->
-    @auth
-        <h2>{{ auth()->user()->email }}</h2>
-    @endauth
+@section('content')
+
+    <div class="p-2 mb-3">
+        <form action="" method="get">
+            <div class="d-flex flex-row justify-content-around">
+                <div class="">
+                    <input class="form-control" type="text" name="" id="" placeholder="Código">
+                </div>
+                <div class="">
+                    <input class="form-control" type="text" name="" id="" placeholder="Descrição">
+                </div>
+                <div class="">
+                    <select class="form-control" name="" id="">
+                        <option value="" selected>Selecione uma opção</option>
+                    </select>
+                </div>
+                <button class="btn btn-primary" type="submit">Filtrar</button>
+            </div>
+        </form>
+    </div>
+
+    <table class="table text-center table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>Código</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Estoque</th>
+                <th>Categoria</th>
+                <th>Imagem</th>
+                <th>Ver</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+        </thead>
+        <tbody>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th><a href=""><i class="fas fa-eye"></i></a></th>
+            <th><a href=""><i class="fas fa-pen"></i></a></th>
+            <th><a href=""><i class="fas fa-trash"></i></a></th>
+        </tbody>
+    </table>
 
 @endsection
