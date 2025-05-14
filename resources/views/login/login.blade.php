423685cd51e78d22b0621ed79ad97d2ee0ab6a78
yuriossugui <yuriossugui2@gmail.com>
2025-05-13 21:38:44 -0300

    style: login form improvment

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,36 +1,14 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-        .login-container {
-            max-width: 400px;
-            margin: 100px auto;
-            padding: 30px;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        .form-group {
-            margin-bottom: 20px;
-        }
-        .btn-primary {
-            width: 100%;
-        }
-        .alert {
-            margin-top: 20px;
-        }
-    </style>
-</head>
-<body>
-    <div class="container login-container">
-        <h2 class="text-center mb-4">Login</h2>
+@extends('layouts.main')
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('assets/css/login.css')}}">
+@endsection
+
+@section('content')
+
+<div class="centered-container">
+    <div class="login-container">
+        <h2 class="text-center mb-4 login-title">Login</h2>
         <form action="{{ route('login.post') }}" method="post">
             @csrf
             <div class="form-group">
@@ -41,7 +19,7 @@
                 <label for="senha" class="form-label">Senha</label>
                 <input type="password" class="form-control" name="password" id="senha" placeholder="Digite sua senha">
             </div>
-            <button type="submit" class="btn btn-primary">Entrar</button>
+            <button type="submit" class="btn btn-primary btn-block">Entrar</button>
         </form>
 
         @if(session('error'))
@@ -59,7 +37,6 @@
             </div>
         @endif
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
